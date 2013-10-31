5e9d525104ca326310f20dd4798e6e5016c50599
jketch836 <jketch@JoshKs-MacBook-Pro.local>
2013-10-31 15:39:54 -0400

    Lab 2
    The story of a Brave Knight and an Evil Dragon in  a Awesome Land.

---
+++ sdi-lab2/Ketcham_Josh_LAB2.js
@@ -0,0 +1,34 @@
+alert("JavaScript Works!");
+
+// Josh Ketcham
+// SDI 1311
+// Lab Day 2
+// Tell Me a Stroy in JavaScript
+
+//First Task
+//My Variables
+var myknight = "GeorgetheBrave";
+var thedragon = "SmaugtheSmellyOne";
+var land = "SuperAwesomeLand\'s";
+var minkill = 5;
+var dragondeath= true;
+var dragonlives= false;
+
+//Second Task
+confirm (" There was once a place called " + land + " land ");
+console.log (land + " people were in peral by " + thedragon);
+console.log (myknight + ' rose up and said "I will slay you" to ' + thedragon);
+
+//Third Task
+confirm (myknight + " traveled " + land + " land and confronted " + thedragon);
+
+confirm (myknight + " faught with " + thedragon + " and slashed him " + minkill + " times. He failed.")
+
+var test= parseInt(prompt (myknight + " faught and slashed at " + thedragon + " again and stabbed him"));
+console.log (myknight + " slashed " + test + " times");
+
+var confirmbox = confirm ("Did " + myknight + " kill " + thedragon +"? ");
+console.log (confirmbox);
+
+console.log (myknight + " killed " + thedragon + " and saved " + land + " people ");
+confirm ("The End");
